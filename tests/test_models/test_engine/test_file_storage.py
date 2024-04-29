74c52f3758199de7d141e02dc7f117de9b019d58
OG-1534 <gershomochanda@gmail.com>
2024-04-29 13:58:44 -0700

    fixing indentation

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,7 +113,7 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
-	
+
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """Tests method for obtaining an instance db storage"""
