38afa96fdd71483a437aefd5eb01eb89582b8d28
OG-1534 <gershomochanda@gmail.com>
2024-04-29 15:52:14 -0700

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,4 +12,4 @@ from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/vi')
+app_views = Blueprint('/api/vi', __name__, url_prefix="/api/vi")
