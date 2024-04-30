cf0e3777ce5b4a4cd16e1df2fc15b0c5016cdfce
nzaumuimi <nzaumuimi@gmail.com>
2024-04-30 02:05:40 -0700

    AirBnB_clone_v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""
-Creating Flask app Blueprint
-"""
-from Flask import Blueprint
+'''
+Create Flask app blueprint
+'''
+from flask import Blueprint
+
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,5 +14,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint('/api/vi', __name__, url_prefix="/api/vi")
